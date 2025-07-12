dc549be4659f91cce98b9b46245ca3411f97e61a
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-07-12 10:12:43 +0630

    update

--- UninstallSecurityUpdates.bat
+++ UninstallSecurityUpdates.bat
@@ -3,7 +3,7 @@ echo Removing startup shortcut...
 del "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\SecurityService.lnk"
 
 echo Terminating running prank process...
-taskkill /f /im wscript.exe /fi "WINDOWTITLE eq Important System Security Process"
+taskkill /f /im cmd.exe /fi "WINDOWTITLE eq Important System Security Process"
 
 echo Prank removed.
 pause
